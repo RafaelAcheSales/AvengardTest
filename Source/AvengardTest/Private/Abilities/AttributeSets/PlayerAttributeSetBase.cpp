fefab340bc342749a0059ff44c21ef90630abf12
RafaelAcheSales <58531701+RafaelAcheSales@users.noreply.github.com>
2023-11-24 20:05:56 -0300

    Add project files.

---
+++ Source/AvengardTest/Private/Abilities/AttributeSets/PlayerAttributeSetBase.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Abilities/AttributeSets/PlayerAttributeSetBase.h"
+#include "Net/UnrealNetwork.h"
+
+void UPlayerAttributeSetBase::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerAttributeSetBase, Mana, OldMana);
+}
+
+void UPlayerAttributeSetBase::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerAttributeSetBase, MaxMana, OldMaxMana);
+}
+
+void UPlayerAttributeSetBase::OnRep_JumpForce(const FGameplayAttributeData& OldJumpForce)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerAttributeSetBase, JumpForce, OldJumpForce);
+}
+
+void UPlayerAttributeSetBase::OnRep_MaxJumpForce(const FGameplayAttributeData& OldMaxJumpForce)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerAttributeSetBase, MaxJumpForce, OldMaxJumpForce);
+}
+
+void UPlayerAttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerAttributeSetBase, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerAttributeSetBase, MaxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerAttributeSetBase, JumpForce, COND_None, REPNOTIFY_Always);
+}
---
+++ Source/AvengardTest/Public/Abilities/AttributeSets/PlayerAttributeSetBase.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "PlayerAttributeSetBase.generated.h"
+// Uses macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+/**
+ * 
+ */
+UCLASS()
+class AVENGARDTEST_API UPlayerAttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UPlayerAttributeSetBase, Mana)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UPlayerAttributeSetBase, MaxMana)
+
+	UPROPERTY(BlueprintReadOnly, Category = "JumpForce", ReplicatedUsing = OnRep_JumpForce)
+	FGameplayAttributeData JumpForce;
+	ATTRIBUTE_ACCESSORS(UPlayerAttributeSetBase, JumpForce)
+
+	UPROPERTY(BlueprintReadOnly, Category = "JumpForce", ReplicatedUsing = OnRep_MaxJumpForce)
+	FGameplayAttributeData MaxJumpForce;
+	ATTRIBUTE_ACCESSORS(UPlayerAttributeSetBase, MaxJumpForce)
+
+	UFUNCTION()
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+	UFUNCTION()
+	virtual void OnRep_JumpForce(const FGameplayAttributeData& OldJumpForce);
+	UFUNCTION()
+	virtual void OnRep_MaxJumpForce(const FGameplayAttributeData& OldMaxJumpForce);
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+};
